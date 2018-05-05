4824f9b4108277910bb56acec20ba5147bdb7b52
Leon Jacobs <leonja511@gmail.com>
2018-05-05 20:52:38 +0200

    Add nginx Dockerfile

---
+++ README.md
@@ -0,0 +1,5 @@
+![SeAT](http://i.imgur.com/aPPOxSK.png)
+# docker-eveseat-nginx
+
+## This repository contains the SeAT Nginx container Dockerfile
+Please use the main SeAT repository [here](https://github.com/eveseat/seat) for issues.
---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Wait for the database
+while ! mysqladmin ping -hmariadb --silent; do
+
+    echo "MariaDB container might not be ready yet... sleeping..."
+    sleep 10
+done
+
+php /var/www/seat/artisan horizon
